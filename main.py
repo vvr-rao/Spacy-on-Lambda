b0465123dd45ed6335670a19f3290dbf859f34a2
vvr-rao <38475577+vvr-rao@users.noreply.github.com>
2022-11-18 14:21:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,15 @@ def handler(event, context):
     payload = data1['data']
     
     #INSERT NER CODE HERE
-    output = 'Some Output'
+    o = ''
+    nlp2 = spacy.load("model")
+    doc = nlp2(payload)
+    
+    for token in doc:
+        o = o  +  token.text + "  " + token.pos_ + "\n"
     
     #return Output
-    response = {"statusCode": 200, "body": output}
+    response = {"statusCode": 200, "body": o}
     return response
 
     
